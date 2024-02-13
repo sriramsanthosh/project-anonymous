9e015d24d0fdfc93319aecd75672ea9415ee64f7
Itha Sriram Santhosh <isrs444@gmail.com>
2024-02-13 18:02:05 +0530

    updated mediaquery

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -24,12 +24,12 @@ function Home(){
     <main className="text-center">
         <div className='india-button-container'><sub><img className='rocket-icon' src={Rocket} alt="rocket-icon" /> </sub><span className="india-button-container-text">For Indian Users Only</span></div>
         <h1 className = "main-content-heading">Start posting anonymously where no one will judge.</h1>
-        <p className="main-content-heading-quote">Welcome to Stranger discussion forum</p>
+        <div className="main-content-heading-quote">Welcome to Stranger discussion forum</div>
         <button onClick={() => navigate("/sign-up")} className='india-button-container create-your-account'>Create Your Account &nbsp; <i class="fa-solid fa-arrow-right"></i></button>
         <p>Already have account? <button className="login-button" onClick={() => navigate("/login")}>Login</button></p>
     </main>
     <footer>
-        <p className="author-sign"><span className="orange-color">Made with</span> <i class="fa-solid fa-heart"></i>&nbsp;<a className="author-profile-link" href="https://www.linkedin.com/in/sriramsanthosh/" target="_blank">Sriram&nbsp;Santhosh</a></p>
+        <div className="author-sign"><span className="orange-color">Made with</span> <i class="fa-solid fa-heart"></i>&nbsp;<a className="author-profile-link" href="https://www.linkedin.com/in/sriramsanthosh/" target="_blank">Sriram&nbsp;Santhosh</a></div>
     </footer>
     </div>
 }
